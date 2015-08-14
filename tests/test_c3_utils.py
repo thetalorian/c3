ab01434ed43f35f93c231208891dfe6d742c47d2
Jesse Escobedo <jesse@moda.bz>
2015-08-14 10:28:44 -0700

    removing nvdb as it should be it's own package

--- tests/test_c3_utils.py
+++ tests/test_c3_utils.py
@@ -1,56 +1,40 @@
 ''' Tests for c3.utils '''
 
 import os
-import sys
 import c3.utils.accounts
 
 
-def test_invalid_get_account_name():
-    ''' Test invalid get_account_name function. '''
+def test_get_account_name():
+    ''' Test function in c3.utils.accounts '''
     os.environ['AWS_ACCOUNT_ID'] = '123456789011'
     mapfile = os.getcwd() + '/tests/account_aliases_map.txt'
     account = c3.utils.accounts.get_account_name(mapfile=mapfile)
     assert account == None
-
-
-def test_empty_get_account_name():
-    ''' Test no account_id and OS env AWS_ACCOUNT_ID. '''
     os.environ['AWS_ACCOUNT_ID'] = ''
     assert c3.utils.accounts.get_account_name() == False
-
-
-def test_get_account_name():
-    ''' Test get_account_name function. '''
     os.environ['AWS_ACCOUNT_ID'] = '123456789012'
-    mapfile = os.getcwd() + '/tests/account_aliases_map.txt'
     account = c3.utils.accounts.get_account_name(mapfile=mapfile)
     assert account == 'opsqa'
 
 
-def test_invalid_get_account_id():
-    ''' Test invalid get_account_id function. '''
+def test_get_account_id():
+    ''' Test function in c3.utils.accounts '''
     mapfile = os.getcwd() + '/tests/account_aliases_map.txt'
     account = c3.utils.accounts.get_account_id(
         account_name='opsprod', mapfile=mapfile)
     assert account == None
-
-
-def test_env_get_account_id():
-    ''' Test invalid get_account_id function. '''
     os.environ['AWS_ACCOUNT_ID'] == '123456789012'
     account = c3.utils.accounts.get_account_id()
     assert account == '123456789012'
-
-
-def test_get_account_id():
-    ''' Test get_account_id function. '''
-    mapfile = os.getcwd() + '/tests/account_aliases_map.txt'
     account = c3.utils.accounts.get_account_id(
         account_name='opsqa', mapfile=mapfile)
     assert account == '123456789012'
 
 
-def test_invalid_translate_account():
-    ''' Test translate_account function. '''
+def test_translate_account():
+    ''' Test function in c3.utils.accounts '''
     mapfile = os.getcwd() + '/tests/account_aliases_map_FAKE.txt'
     assert c3.utils.accounts.translate_account(mapfile=mapfile) == False
+    os.environ['AWS_CONF_DIR']= os.getcwd() + '/tests'
+    name = c3.utils.accounts.translate_account(account_id='123456789012')
+    assert name == 'opsqa'
