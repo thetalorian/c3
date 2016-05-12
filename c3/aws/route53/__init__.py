0466c1e72408703898e9048e5c8accb78137808f
Jesse Escobedo <jesse@moda.bz>
2016-05-11 21:18:50 -0700

    updating copyright

--- c3/aws/route53/__init__.py
+++ c3/aws/route53/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2015 CityGrid Media, LLC
+# Copyright 2016 CityGrid Media, LLC
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
